accd159d646a574c034256844df178db9aec29a3
Bruno Ancona <brunoanconasala@gmail.com>
2026-01-21 10:04:02 -0600

    Task 19

---
+++ SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- Display average temp of top 3 cities on July and August
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
